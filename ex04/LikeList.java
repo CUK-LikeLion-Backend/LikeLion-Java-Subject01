52fde1bde4bf7d3370dc3cb1e7ca82dc2182acb6
jjindol <andy15948@naver.com>
2023-05-09 13:33:46 +0900

    Docs: 자바 과제

--- ex04/LikeList.java
+++ ex04/LikeList.java
@@ -2,29 +2,29 @@ package ex04;
 
 public class LikeList {
 
-    private Node head;
-    private int size;
+    private Node head; // 연결 리스트의 첫 번째 노드
+    private int size; // 연결 리스트에 저장된 노드의 수
 
     public class Node {
         int data;
-        Node next;
+        Node next; // 다음 노드를 가리키는 참조 변수
 
         Node(int data) {
             this.data = data;
             next = null;
         }
-    }
+    } // head = null, size = 0
 
     public void insert(int index, int data) {
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException();
-        }
+        } // 인덱스의 유효성 판단, 유효하지 않으면 예외를 발생 시킴
 
         Node newNode = new Node(data);
 
-        if (index == 0) {
-            newNode.next = head;
-            head = newNode;
+        if (index == 0) { // 삽입 위치가 맨 앞
+            newNode.next = head; // 'head' 변수를 새로운 노드로 바꾸고
+            head = newNode; // 새 노드의 'next' 변수를 이전 'head' 노드로 설정
         } else {
             Node current = head;
             for (int i = 0; i < index - 1; i++) {
@@ -34,7 +34,7 @@ public class LikeList {
             current.next = newNode;
         }
 
-        size++;
+        size++; // 리스트의 크기 1 증가
     }
 
     public void delete(int index) {
@@ -54,15 +54,15 @@ public class LikeList {
 
         size--;
     }
-
     public void print() {
         Node current = head;
-        while (current != null) {
-            System.out.print(current.data + " ");
-            current = current.next;
+        while (current != null) { // current 노드 변수가 null이 아닐 때까지 반복
+            System.out.print(current.data + " "); // null이 되면 탐색 종료
+            current = current.next; // 변수를 다음 노드로 이동
         }
         System.out.println();
     }
+
     public static void main(String[] args) {
         LikeList list = new LikeList();
 
--- ex05/LikeStack.java
+++ ex05/LikeStack.java
@@ -8,15 +8,15 @@ public class LikeStack {
         list = new LikeList();
     }
 
-    public void push(int value) {
-        list.insert(0, value);
+    public void push(int value) { // 맨 앞에 새로운 노드 삽입
+        list.insert(0, value); // 새로운 노드를 head에 추가
     }
 
     public void pop() {
         list.delete(0);
     }
 
-    public int peek() {
+    public int peek() { // 스택의 맨 위에 있는 값을 반환
         return list.head.data;
     }
 
@@ -32,12 +32,15 @@ public class LikeStack {
         stack.push(2);
         stack.push(3);
 
+        // 스택에서 데이터 삭제
+        stack.pop();
+
         // 스택 출력
         while (!stack.isEmpty()) {
             System.out.print(stack.peek() + " ");
             stack.pop();
         }
-        System.out.println(); // 출력: 3 2 1
+        System.out.println(); // 출력: 2 1
     }
 
 }
